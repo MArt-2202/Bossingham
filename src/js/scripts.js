58086cd978715b6615357d2051f344d2099fd58f
Artem <mart_22@ukr.net>
2023-05-25 00:20:21 +0300

    Import, init lightbox in scripts.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,8 +5,9 @@ import modal from './modules/modal';
 import blocksStyles from './modules/blocksStyles';
 import toggleContent from './modules/toggleContent';
 import blockPosition from './modules/blockPosition';
-import sliders from './modules/sliders';
+import lightbox from './modules/lightbox';
 import sendFormData from './modules/sendFormData';
+import scrollAnimateNodes from './modules/scrollAnimateNodes';
 
 if ('ontouchstart' in document.documentElement) {
 	document.body.classList.add('touchdevice');
@@ -27,7 +28,27 @@ if (isMobile()) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-	sliders();
+	scrollAnimateNodes({
+		node: 'h1, h2',
+		animatedClass: 'slideInLeft',
+	});
+	scrollAnimateNodes({
+		node: '.animate-3 > div',
+		animatedClass: 'slideInUp',
+	});
+	scrollAnimateNodes({
+		node: 'main p, .top-section__add .btn, .top-section__add .modal-btn, .top-section__img, .about-us__img, .breadcrumbs li, .info__btn',
+		animatedClass: 'fadeIn',
+	});
+	lightbox({
+		container: '#info__lightbox',
+		selector: '.info-slider__item',
+		plugins: [lgZoom],
+		speed: 500,
+		zoom: true,
+		download: false,
+		mode: 'lg-fade',
+	});
 	sendFormData({
 		closeBtn: '',
 		hiddenNodes: '[data-modal="modal-1"] .modal__info',
