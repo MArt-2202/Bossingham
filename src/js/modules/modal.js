cb1e7d26d73480d40bfd63f6349be979040e1a6c
Artem <mart_22@ukr.net>
2023-05-24 15:49:31 +0300

    Edit modal.js

--- src/js/modules/modal.js
+++ src/js/modules/modal.js
@@ -1,4 +1,6 @@
 export default function modal() {
+	let modalIsVisible = false;
+
 	if (document.querySelector('.modal-btn')) {
 		document.querySelectorAll('.modal-btn').forEach((el) => {
 			el.addEventListener('click', function (e) {
@@ -8,6 +10,16 @@ export default function modal() {
 					const targetModal = document.querySelector(`[data-modal="${el.dataset.modalBtn}"]`);
 
 					targetModal.classList.remove('dn');
+
+					if (targetModal.querySelector('.form__message')) {
+						targetModal.querySelector('.form__message').style.display = '';
+					}
+					if (targetModal.querySelector('.hidden')) {
+						targetModal
+							.querySelectorAll('.hidden')
+							.forEach((el) => el.classList.remove('hidden'));
+					}
+					modalIsVisible = true;
 					setTimeout(() => {
 						targetModal.classList.add('show');
 					}, 50);
@@ -15,6 +27,22 @@ export default function modal() {
 					document.body.style.paddingRight =
 						window.innerWidth - document.documentElement.clientWidth + 'px';
 					document.body.style.overflowY = 'hidden';
+
+					if (modalIsVisible) {
+						window.addEventListener('keydown', function (e) {
+							if ((e?.key && e.key === 'Escape') || (e?.code && e.code === 'Escape')) {
+								targetModal.classList.remove('show');
+								setTimeout(() => {
+									targetModal.classList.add('dn');
+								}, 200);
+
+								document.body.style.overflowY = '';
+								document.body.style.paddingRight = '';
+							}
+						});
+					} else {
+						return;
+					}
 				}
 			});
 		});
@@ -28,19 +56,24 @@ export default function modal() {
 				e.stopPropagation();
 
 				if (!target.closest('.modal__content') || target.closest('.modal__close')) {
+					if (document.querySelector('.modal-overlay.show .form__message')) {
+						document.querySelector('.modal-overlay.show .form__message').style.display = '';
+					}
+					if (document.querySelector('.modal-overlay.show .hidden')) {
+						document
+							.querySelectorAll('.modal-overlay.show .hidden')
+							.forEach((el) => el.classList.remove('hidden'));
+					}
+
+					modalIsVisible = false;
 					el.classList.remove('show');
+
 					setTimeout(() => {
 						el.classList.add('dn');
 					}, 200);
 
 					document.body.style.overflowY = '';
 					document.body.style.paddingRight = '';
-
-					if (document.querySelector('#modal__success')) {
-						document
-							.querySelector('#modal__success')
-							.classList.remove('modal__success--visible');
-					}
 				}
 			});
 		});
